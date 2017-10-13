880166bf6f06cd2467b22028fb472fcff59dac27
ramses7083 <ramses7083@gmail.com>
2017-10-13 17:34:12 -0500

    Initial Commit

---
+++ ReBUC Ramses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReBUC Ramses
+//
+//  Created by Ramses Miramontes Meza on 13/10/17.
+//  Copyright © 2017 Ramses Miramontes Meza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
